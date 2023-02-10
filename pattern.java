aee01fe813a55a5bfb2c11ac175b082fed8a2f1c
santosh1253 <konkimalla1253@gmail.com>
2023-02-10 11:03:22 +0530

    import java.util.Scanner; public class MyClass {     public static void main(String args[]) {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();       for(int i=0;i<n;i++)       {           String x=" ";           System.out.print(x.repeat(n-i));           for(int j=0;j<n;j++)           {               System.out.print("*"+" ");           }           System.out.println();       }   } }

---
+++ pattern.java
@@ -0,0 +1,18 @@
+// Trapezium pattern
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+      for(int i=0;i<n;i++)
+      {
+          String x=" ";
+          System.out.print(x.repeat(n-i));
+          for(int j=0;j<n;j++)
+          {   
+              System.out.print("*"+" "); 
+          }
+          System.out.println();
+      }
+  }
+}
